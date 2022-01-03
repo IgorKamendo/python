76fdae03fc98901ddebfa8819395d77edc0e2af2
Igor <coolstorm2011@gmail.com>
2022-01-03 23:56:39 +0300

    first commit

---
+++ M_1_3.py
@@ -0,0 +1,5 @@
+x = float(input("Введите первое число"))
+y = float(input("Введите второе число"))
+z = (x > y)*x+(x <= y)*y
+print(z)
+
---
+++ M_2_1.py
@@ -0,0 +1,10 @@
+x = int(input("Введите первое число "))
+y = int(input("Введите второе число "))
+z = int(input("Введите третье число "))
+
+if x == y and y == z and x == z:
+    print("3")
+elif x == y or y == z or x == z:
+    print("2")
+else:
+    print("0")
